452e5d1841ea7bd3e5b394840cf0f86fb07e2172
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-02-24 16:38:47 -0500

    Ignore all output files

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,7 @@ set -o errexit
 # For local convertsion to PDF without any citations use:
 # pandoc --verbose --from=markdown --output=review.pdf sections/*.md
 
+# Generate reference information
 (cd build && python references.py)
 
 # pandoc settings
@@ -10,13 +11,16 @@ CSL_PATH=https://github.com/citation-style-language/styles/raw/master/dependent/
 BIBLIOGRAPHY_PATH=references/generated/bibliography.json
 INPUT_PATH=references/generated/all-sections.md
 
+# Make output directory
+mkdir -p output
+
 # Create HTML outpout
 # http://pandoc.org/MANUAL.html
 pandoc --verbose \
   --from=markdown --to=html \
   --bibliography=$BIBLIOGRAPHY_PATH \
   --csl=$CSL_PATH \
-  --output=review.html \
+  --output=output/index.html \
   $INPUT_PATH
 
 # Create PDF outpout
@@ -25,5 +29,5 @@ pandoc \
   --to=html5 \
   --bibliography=$BIBLIOGRAPHY_PATH \
   --csl=$CSL_PATH \
-  --output=review.pdf \
+  --output=output/deep-review.pdf \
   $INPUT_PATH
