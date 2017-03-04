greenelab/deep-review

commit 3b763ff72b4607c95ad3df05b3a1cb366fa901c8

Timestamp manuscripts with OpenTimestamps (#274)
Closes https://github.com/greenelab/deep-review/issues/269

ci/deploy.sh
@@ -1,4 +1,14 @@
+# Exit on errors
 set -o errexit
+
+# Add commit hash to the README
+envsubst < output/README.md > output/README.md
+
+# Generate OpenTimestamps
+python ci/opentimestamps-client/ots stamp \
+  output/index.html \
+  output/deep-review.pdf \
+  output/README.md
 
 # Configure git
 git config --global push.default simple

.gitignore
@@ -1,6 +1,7 @@
 # Generated manuscript output files
 output/index.html
 output/deep-review.pdf
+output/*.ots
 
 # Generated reference files
 references/generated/*

.gitmodules
@@ -1,0 +1,3 @@
+[submodule "ci/opentimestamps-client"]
+	path = ci/opentimestamps-client
+	url = https://github.com/opentimestamps/opentimestamps-client.git

build/environment.yml
@@ -12,3 +12,4 @@ dependencies:
     - arxiv2bib==1.0.7
     - bibtexparser==0.6.2
     - ghp-import==0.5.5
+    - python-bitcoinlib==0.7.0

commit 2d589e4839be73e08ff8a8870b731a729fc671ca

Fix blank output/README.md bug (#275)
Affects gh-pages branch only.

Fixed to allow reading from and writing to the same file.
See http://stackoverflow.com/a/3897483/4651668

Bug introduced in https://github.com/greenelab/deep-review/pull/274.

ci/deploy.sh
@@ -2,7 +2,8 @@
 set -o errexit
 
 # Add commit hash to the README
-envsubst < output/README.md > output/README.md
+envsubst < output/README.md > output/README-complete.md
+mv output/README-complete.md output/README.md
 
 # Generate OpenTimestamps
 python ci/opentimestamps-client/ots stamp \