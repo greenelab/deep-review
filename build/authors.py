f26bc5cde8ebd0b5191c7da43243a014eb9d1162
Anthony Gitter <gitter@biostat.wisc.edu>
2017-06-19 10:51:57 -0500

    Add functions to format funding information for templates

--- build/authors.py
+++ build/authors.py
@@ -2,9 +2,13 @@
 Process author table
 """
 
-import collections
 import pandas
 import numpy as np
+
+from collections import (
+    defaultdict,
+    OrderedDict,
+)
 
 
 def initials(full_name):
@@ -16,6 +20,46 @@ def initials(full_name):
     # Keeps only the first part of hyphenated names
     name_tokens = [name[0] + '.' for name in name_tokens]
     return ''.join(name_tokens)
+
+def format_list(tokens):
+    """
+    Format a list of string tokens such that a pair [X, Y] is formatted 'X and
+    Y' and a list [X, Y, Z] becomes 'X, Y, and Z'.  Single tokens are returned
+    as-is.
+    """
+    if len(tokens) <= 2:
+        return ' and '.join(tokens)
+    else:
+        tokens[-1] = 'and ' + tokens[-1]
+        return ', '.join(tokens)
+
+def get_funding(author_df):
+    """
+    Returns a dictionary mapping funders ('GBMF', 'NIH', 'NSF', 'OTHER') to
+    award and author text.  An author may have more than one funding
+    source and multiple authors may have the same funding source.
+    """
+    # Initially create a map from funders to dictionaries that map an award to
+    # the authors supported by that award
+    funding_info = defaultdict(lambda: defaultdict(list))
+    for index, row in author_df[author_df['funding'].notnull()].iterrows():
+        for grant in row['funding'].split(','):
+            # Separate into the funder and the award
+            grant_tokens = grant.strip().split(None, 1)
+            funder_map = funding_info[grant_tokens[0]]
+            funder_map[grant_tokens[1]].append(row['initials'])
+
+    formatted_info = OrderedDict()
+    # Sort because the defaultdict is not ordered
+    for funder in sorted(funding_info.keys()):
+        funder_map = funding_info[funder]
+        funder_awards = list()
+        for award in sorted(funder_map.keys()):
+            authors = sorted(funder_map[award])
+            funder_awards.append(f'{award} ({format_list(authors)})')
+        formatted_info[funder] = format_list(funder_awards)
+
+    return formatted_info
 
 def get_author_info(author_file):
     """
@@ -35,6 +79,9 @@ def get_author_info(author_file):
         'All contributors must approve of the manuscript before being ' + \
         'added as authors'
 
-    author_info = collections.OrderedDict()
+    author_info = OrderedDict()
     author_info['count'] = len(author_df)
+
+    author_info.update(get_funding(author_df))
+
     return author_info
