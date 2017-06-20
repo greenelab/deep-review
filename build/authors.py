a4eab38ca9363b43b479a98dd7f3fa4f6ade6f86
Anthony Gitter <gitter@biostat.wisc.edu>
2017-06-20 09:06:46 -0500

    Refactor affiliation code and add annotation to first author

--- build/authors.py
+++ build/authors.py
@@ -41,6 +41,8 @@ def get_funding(author_df):
     """
     # Initially create a map from funders to dictionaries that map an award to
     # the authors supported by that award
+    # funding_info data structure has the form
+    # {funder1: {award1: [author1], award2: [author2, author3]}}
     funding_info = defaultdict(lambda: defaultdict(list))
     for index, row in author_df[author_df['funding'].notnull()].iterrows():
         for grant in row['funding'].split(','):
@@ -52,6 +54,7 @@ def get_funding(author_df):
     formatted_info = OrderedDict()
     # Sort because the defaultdict is not ordered
     for funder in sorted(funding_info.keys()):
+        # The map from awards to author lists for a single funder
         funder_map = funding_info[funder]
         funder_awards = list()
         for award in sorted(funder_map.keys()):
@@ -78,23 +81,27 @@ def get_affiliations(author_df):
         name = row['full_name']
         affiliation = row['affiliation']
 
-        # Lookup the affiliation index
+        # Lookup the affiliation index or create it
+        # Only list each affiliation once
         if not affiliation in affiliation_map:
             affiliation_map[affiliation] = affiliation_counter
+            affiliations.append(f'{affiliation_counter}. {affiliation}')
             affiliation_counter += 1
-            add_affiliation = True
         index = affiliation_map[affiliation]
 
         # Add the affiliation superscript
-        name = f'{name}<sup>{index}</sup>'
+        # Assumes a single affiliation per author (which could contain multiple
+        # institutions) but could be generalized
+        # Do not add the close </sup> tag because some authors have special
+        # annotations about the author ordering or correspondence
+        name = f'{name}<sup>{index}'
         author_names.append(name)
 
-        # Only list each affiliation once
-        if add_affiliation:
-            affiliations.append(f'{index}. {affiliation}')
-            add_affiliation = False
+    # Add special annotation for the first listed author
+    author_names[0] = author_names[0] + ',*'
+    author_names = [name + '</sup>' for name in author_names]
 
-    affiliation_info = OrderedDict()
+    affiliation_info = OrderedDict()  
     affiliation_info['authors'] = ',\n'.join(author_names)
     affiliation_info['affiliations'] = '\n'.join(affiliations)
     return affiliation_info
