cf24601f506b61289a8b1407567a265ba9afbd8a
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-06-29 15:38:45 -0400

    Disable author parsing

--- build/references.py
+++ build/references.py
@@ -25,7 +25,7 @@ from citations import (
     validate_reference,
 )
 
-from authors import get_author_info
+# from authors import get_author_info
 
 # Run only as a script
 assert __name__ == '__main__'
@@ -154,7 +154,7 @@ print(ref_counts)
 
 # Author table information
 authors_path = pathlib.Path('../authors.tsv')
-stats['authors'] = get_author_info(authors_path)
+# stats['authors'] = get_author_info(authors_path)
 
 with gen_dir.joinpath('stats.json').open('wt') as write_file:
     json.dump(stats, write_file, indent=2)
