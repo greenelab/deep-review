8443b3c075b00a659073147d3ab0b45186fe2b7b
wytemike <50783497+MoneyMan573@users.noreply.github.com>
2022-12-24 17:57:53 -0600

    Update autobuild.sh

--- build/autobuild.sh
+++ build/autobuild.sh
@@ -1,9 +1,25 @@
-#!/usr/bin/env bash
+///#
+~_-$./
+"lostleolotus/make-s6lx9_microboard_defconfig-
+###
+"buildroot"-
+###
+$/github-
+_buildroot-org-root-user-MoneyMan573-main-root-buildroot.org/system/skeleton/root/user-MoneyMan573-teslamotors/buildroot.org"
+./"make-$-cp_build"
+"
+~_-$.
+/"
+/"linux-<version>/arch/microblaze/boot/simpleImage.lx9_mmu.ub/tftpboot/br12.2a.ub_minicom"
+"/usr/bin/env"
+bash
 
-## autobuild.sh: automatically rebuild mansucript outputs and the webpage when content changes
-## Depends on watchdog https://github.com/gorakhargosh/watchdog
+///## 
+"autobuild.sh: automatically rebuild mansucript outputs and the webpage when content changes
+## Depends on watchdog https://github.com/gorakhargosh/watchdog"
 
-watchmedo shell-command \
+///###...___---$$$///"""
+"""watchmedo shell-command \
   --wait \
   --command='bash build/build.sh && manubot webpage' \
   content
